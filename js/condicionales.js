154a0a5a1ad8a4cbb7a554c701ce68b2728ea035
MarcosIvanPuggioni <ericdreadmar@hotmail.com>
2021-10-30 02:34:30 -0300

    first commit

---
+++ js/condicionales.js
@@ -0,0 +1,15 @@
+nombre = prompt("Ingresa tu Nombre:")
+edad = Number(prompt("Ingresa tu edad:"))
+
+if (nombre == "marcos"){
+    alert("Bienvenido " + nombre)
+}else 
+alert("Datos incorrectos , vos no te llamas marcos")
+
+if (edad >= 18){
+    alert("Tienen edad para un tatuaje")
+}else if (edad >= 16){
+alert("Tienes que venir con un adulto que autorize tatuarte")
+}else 
+    alert("De ninguna manera tenes edad para tatuarte")
+
---
+++ js/variables.js
@@ -0,0 +1,11 @@
+/*
+let nombre = prompt("Ingresa tu nombre")
+let apellido = prompt("Ingresa tu apellido")
+let edad = prompt("Ingresa tu edad")
+let numeroEdad = Number(edad)
+let añosMayor = 10
+let edad10Años = numeroEdad + añosMayor
+console.log("Tu nombre es: " + nombre)
+console.log("tu apellido es: " + apellido)
+console.log("tu edad es: " + edad)
+alert("En 10 años vas a ser un programador full stack y vas a tener " + edad10Años) */

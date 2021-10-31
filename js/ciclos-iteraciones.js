873f77b475c171cc1ee44250c7446a092eaedd66
MarcosIvanPuggioni <ericdreadmar@hotmail.com>
2021-10-30 22:25:32 -0300

    CICLOSEITERACIONESREAL

---
+++ js/ciclos-iteraciones.js
@@ -0,0 +1,9 @@
+let pass = "";
+
+do {
+    pass = prompt("Introduzca la palabra clave")
+    if ( pass != "river plate" ){
+        alert("Una pista , le gano a boca juniors en madrid")
+    }
+} while (pass != "river plate")
+alert("9 diciembre, 2018")
